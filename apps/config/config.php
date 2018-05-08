1282246cbea12b047db910b889f8a0074b864d04
zhangguichuan <zhangguichuan@youxiduo.com>
2018-05-08 10:17:33 +0800

    change

--- apps/config/config.php
+++ apps/config/config.php
@@ -16,7 +16,7 @@ return array(
 		'host' => '127.0.0.1',
 		'port' => '3306',
 		'username' => 'root',
-		'password' => 'root',
+		'password' => '123456',
         'charset'=>'utf8',
 		'dbname' => 'jiaoben',
 
