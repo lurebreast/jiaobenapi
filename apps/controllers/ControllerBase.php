57b6a7373d808c65d3a53060f40c3f958917abf1
zhangguichuan <zhangguichuan@youxiduo.com>
2018-06-04 15:39:44 +0800

    手机适配

--- apps/controllers/ControllerBase.php
+++ apps/controllers/ControllerBase.php
@@ -9,12 +9,55 @@ abstract class ControllerBase extends \Phalcon\Mvc\Controller
         $this->view->setVar('config', $this->config);
         $this->view->setVar("domain_url", $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST']);
     }
-    public function afterExecuteRoute() {
-        $this->assets->addCss('css/bootstrap.min.css')
-            ->addCss('css/mdadmin.css?v=1');
-        $this->assets->addJs('js/jquery.js')
-            ->addJs('js/bootstrap.min.js');
+    public function afterExecuteRoute()
+    {
+        if (!$this->session->get('admin_auth')) {
+            $this->assets
+                ->addCss('static/css/bootstrap.min.css')
+                ->addCss('static/css/bootstrap-theme.min.css')
+                ->addCss('static/css/pnotify.min.css')
+                ->addJs('static/js/jquery.min.js')
+                ->addJs('static/js/bootstrap.min.js')
+                ->addJs('static/js/pnotify.min.js');
+        } else {
+            $this->assets
+                ->addCss('static/css/bootstrap.min.css')
+                ->addCss('static/css/font-awesome.min.css')
+                ->addCss('static/css/ionicons.min.css')
+                ->addCss('static/css/adminlte.min.css')
+                ->addCss('static/css/skin-black.min.css')
+                ->addCss('static/css/blue.css')
+                ->addCss('static/css/bootstrap-datetimepicker.min.css')
+                ->addCss('static/css/jquery-ui.css')
+                ->addCss('static/css/select2.css')
+                ->addCss('static/css/select2-bootstrap.min.css')
+                ->addCss('static/css/bootstrap-editable.css')
+                ->addCss('static/css/styles.css')
+                ->addCss('static/css/layout.css')
+                ->addCss('static/css/tree.css')
+                ->addJs('static/js/jquery.min.js')
+                ->addJs('static/js/jquery.scrollto.min.js')
+                ->addJs('static/js/moment.min.js')
+                ->addJs('static/js/jquery-ui.min.js')
+                ->addJs('static/js/jquery-ui-i18n.min.js')
+                ->addJs('static/js/bootstrap.min.js')
+                ->addJs('static/js/bootstrap-datetimepicker.min.js')
+                ->addJs('static/js/jquery.form.js')
+                ->addJs('static/js/jquery.confirmexit.js')
+                ->addJs('static/js/bootstrap-editable.min.js')
+                ->addJs('static/js/select2.min.js')
+                ->addJs('static/js/app.min.js')
+                ->addJs('static/js/icheck.min.js')
+                ->addJs('static/js/jquery.slimscroll.min.js')
+                ->addJs('static/js/jquery.waypoints.min.js')
+                ->addJs('static/js/sticky.min.js')
+                ->addJs('static/js/readmore.min.js')
+                ->addJs('static/js/masonry.pkgd.min.js')
+                ->addJs('static/js/admin.js')
+                ->addJs('static/js/treeview.js');
+        }
     }
+
     public function getUser($feild = NULL, $default = NULL) {
         if ($this->session->get('identity')) {
             $this->_user = \Admin::findFirst($this->session->get('identity'));
