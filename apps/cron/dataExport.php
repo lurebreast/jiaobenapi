250d20e565db55c94f3fe1dbb6ebec01eb144089
shajiaming <shajm@shsunedu.com>
2018-10-18 19:07:12 +0800

    到处

--- apps/cron/dataExport.php
+++ apps/cron/dataExport.php
@@ -28,7 +28,7 @@ if (mysqli_connect_errno()) {
 
 $json = $redis->lPop('data_export');
 
-//$json = '{"_url":"\/typedata\/outdata","typeid":"3448","status":"0","sttime":"","endtime":"","image_file":"0"}';
+//$json = '{"_url":"\/typedata\/outdata","typeid":"3448","status":"0","sttime":"","endtime":"","image_file":"1"}';
 if (!$json) {
     $mysqli->close();
     $redis->close();
@@ -59,6 +59,10 @@ $limit = 1000;
 $limitMerge = $arr['image_file'] ? 30 : 300; // 导出图片3万一个文件 图片地址30万一个文件 = $limit x $limitMerge
 
 $times = ceil($total / $limit);
+
+
+//$limit = 1;
+//$times = 2;
 
 $redis->hset('data_export_'.$arr['typeid'], 'complete', $times);
 
@@ -95,14 +99,27 @@ for ($i = 0; $i < $times; $i++) {
         $objActSheet->setCellValue('B1', '提取');
         $objActSheet->setCellValue('C1', '项目id');
         $objActSheet->setCellValue('D1', '项目名称');
-        $objActSheet->setCellValue('E1', '图片');
-        $objActSheet->setCellValue('F1', '图片1');
-        $objActSheet->setCellValue('G1', '上传时间');
-        $objActSheet->setCellValue('H1', '更新时间');
-        $objActSheet->setCellValue('I1', '数据');
-// 设置个表格宽度
-        $objActSheet->getColumnDimension('E')->setWidth(30);
-        $objActSheet->getColumnDimension('F')->setWidth(30);
+        $objActSheet->setCellValue('E1', '更新时间');
+        $objActSheet->setCellValue('F1', '上传时间');
+        $objActSheet->setCellValue('G1', '数据');
+        $objActSheet->setCellValue('H1', '图片');
+        $objActSheet->setCellValue('I1', '图片1');
+        // 设置个表格宽度
+        $objActSheet->getColumnDimension('E')->setWidth(19);
+        $objActSheet->getColumnDimension('F')->setWidth(19);
+        $objActSheet->getColumnDimension('G')->setWidth(50);
+
+        if ($arr['image_file']) {
+            $objActSheet->getColumnDimension('H')->setWidth(6);
+            $objActSheet->getColumnDimension('I')->setWidth(6);
+        } else {
+            $objActSheet->getColumnDimension('H')->setWidth(44);
+            $objActSheet->getColumnDimension('I')->setWidth(44);
+        }
+
+        $objActSheet->getStyle('H')->getAlignment()->setWrapText(true);
+        $objActSheet->getStyle('G')->getAlignment()->setWrapText(true);
+        $objActSheet->getStyle('I')->getAlignment()->setWrapText(true);
     }
 
     $sql = "select * from typedata where id<".$maxId." and $where";
@@ -121,53 +138,53 @@ for ($i = 0; $i < $times; $i++) {
         $objActSheet->setCellValue('B'.$k, $row['status'] == 1 ? '未提取' : '已提取');
         $objActSheet->setCellValue('C'.$k, $row['tid']);
         $objActSheet->setCellValue('D'.$k, $typename);
+        $objActSheet->setCellValue('E'.$k, date('Y-m-d H:i:s', $row['creattime']));
+        $objActSheet->setCellValue('F'.$k, $row['updatetime'] ? date('Y-m-d H:i:s', $row['updatetime']) : '');
+        $objActSheet->setCellValue('G'.$k, $row['data']);
 
-//        $row['img'] = $row['img1'] = '';
+        //        $row['img'] = $row['img1'] = '';
         if ($arr['image_file']) {
             // 图片生成
+
             $image_dir = __DIR__ . '/../../public';
             if ($row['img'] && file_exists($image_dir.$row['img'])) {
                 $objDrawing[$k] = new \PHPExcel_Worksheet_Drawing();
                 $objDrawing[$k]->setPath($image_dir.$row['img']);
                 // 设置宽度高度
-                $objDrawing[$k]->setHeight(400);//照片高度
-                $objDrawing[$k]->setWidth(200); //照片宽度
+                $objDrawing[$k]->setWidth(30); //照片宽度
+                $objDrawing[$k]->setResizeProportional(true);
                 /*设置图片要插入的单元格*/
-                $objDrawing[$k]->setCoordinates('E'.$k);
+                $objDrawing[$k]->setCoordinates('H'.$k);
                 // 图片偏移距离
                 $objDrawing[$k]->setOffsetX(6);
                 $objDrawing[$k]->setOffsetY(6);
                 $objDrawing[$k]->setWorksheet($objPHPExcel->getActiveSheet());
             } else {
-                $objActSheet->setCellValue('E'.$k, '');
+                $objActSheet->setCellValue('H'.$k, '');
             }
             if ($row['img1'] && file_exists($image_dir.$row['img1'])) {
                 $objDrawing[$k] = new \PHPExcel_Worksheet_Drawing();
                 $objDrawing[$k]->setPath($image_dir.$row['img1']);
                 // 设置宽度高度
-                $objDrawing[$k]->setHeight(400);//照片高度
-                $objDrawing[$k]->setWidth(200); //照片宽度
+                $objDrawing[$k]->setWidth(30); //照片宽度
+                $objDrawing[$k]->setResizeProportional(true);
                 /*设置图片要插入的单元格*/
-                $objDrawing[$k]->setCoordinates('F'.$k);
+                $objDrawing[$k]->setCoordinates('I'.$k);
                 // 图片偏移距离
                 $objDrawing[$k]->setOffsetX(6);
                 $objDrawing[$k]->setOffsetY(6);
                 $objDrawing[$k]->setWorksheet($objPHPExcel->getActiveSheet());
             } else {
-                $objActSheet->setCellValue('F'.$k, '');
+                $objActSheet->setCellValue('I'.$k, '');
             }
         } else {
-            $objActSheet->setCellValue('E'.$k, ($row['img'] ? 'http://47.99.122.175'.$row['img'] : ''));
-            $objActSheet->setCellValue('F'.$k, ($row['img1'] ? 'http://47.99.122.175'.$row['img1'] : ''));
+            $objActSheet->setCellValue('H'.$k, ($row['img'] ? 'http://47.99.122.175'.$row['img'] : ''));
+            $objActSheet->setCellValue('I'.$k, ($row['img1'] ? 'http://47.99.122.175'.$row['img1'] : ''));
         }
-
-        $objActSheet->setCellValue('G'.$k, date('Y-m-d H:i:s', $row['creattime']));
-        $objActSheet->setCellValue('H'.$k, $row['updatetime'] ? date('Y-m-d H:i:s', $row['updatetime']) : '');
-        $objActSheet->setCellValue('I'.$k, $row['data']);
 
         // 表格高度
         if ($arr['image_file']) {
-            $objActSheet->getRowDimension($k)->setRowHeight(300);
+            $objActSheet->getRowDimension($k)->setRowHeight(50);
         }
 
         $maxId = $row['id'];
@@ -194,6 +211,7 @@ for ($i = 0; $i < $times; $i++) {
     usleep(200000);
 }
 
+//var_dump($file_arr);
 echo " 导出完成".json_encode($file_arr).PHP_EOL;
 
 $redis->hset('data_export_'.$arr['typeid'], 'lock', 0);
