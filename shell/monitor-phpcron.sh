3a00f10102d3d4b99a084a3f4bede038d5034086
zhangguichuan <lurebreast@126.com>
2019-04-20 10:46:05 +0800

    demo

--- shell/monitor-phpcron.sh
+++ shell/monitor-phpcron.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#! /bin/sh
 ps -fe|grep php-cron.sh |grep -v grep
 if [ $? -ne 0 ]
 then
     echo "start process....."
-    nohup /home/wwwroot/default/shell/php-cron.sh &
+    /bin/sh /home/wwwroot/default/shell/php-cron.sh &
 else
     echo "runing....."
 fi
