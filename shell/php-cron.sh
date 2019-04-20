4db2342c986f2cc14cfbbd15e7a12173cada039c
zhangguichuan <lurebreast@126.com>
2019-04-20 10:20:04 +0800

    demon 时间调整

--- shell/php-cron.sh
+++ shell/php-cron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while [ true ]; do
     /bin/sleep 3
-    php /home/wwwroot/default/apps/cron/queue.php > /dev/null &
+    php /home/wwwroot/default/apps/cron/queue.php >> /tmp/phpcron.log &
 done
