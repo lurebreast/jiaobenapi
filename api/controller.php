eb054c1cf0b9df77159179da03eb8de329788ee3
zhanggc <zhanggc@shsunedu.com>
2019-04-25 11:26:05 +0800

    编码日志

--- api/controller.php
+++ api/controller.php
@@ -47,6 +47,7 @@ function action_set($request)
 
     $encode = mb_detect_encoding($data, array('ASCII','UTF-8','GB2312','GBK'));
     if ($encode != 'UTF-8'){
+        file_put_contents("/tmp/charse.txt", date("Y-m-d H:i:s ").$encode.": ".$data);
         $data = iconv($encode,'UTF-8',$data);
     }
 
