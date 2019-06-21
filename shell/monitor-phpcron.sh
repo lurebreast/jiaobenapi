6a86af52718a880404a6d26e389c8262aa1e71f5
zhanggc <zhanggc@shsunedu.com>
2019-06-21 10:50:31 +0800

    进程监控由定时任务改成supersor

--- shell/monitor-phpcron.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-ps -fe|grep php-cron.sh |grep -v grep
-if [ $? -ne 0 ]
-then
-    echo "start process....."
-    /bin/sh /home/wwwroot/default/shell/php-cron.sh &
-else
-    echo "runing....."
-fi
